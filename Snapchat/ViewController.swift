e8a4e47908fb7c00a745f23b2cd352a5cd696a6f
Joshua Cox <joshua.n.cox@gmail.com>
2015-05-27 13:49:31 -0700

    Initial Commit

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by Joshua Cox on 5/27/15.
+//  Copyright (c) 2015 Joshua Cox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
